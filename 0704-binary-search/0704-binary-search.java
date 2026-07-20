9ba195321f75b405d559f14153883ef0f11dda04
tejasnag189 <tejasnag189@gmail.com>
2026-07-21 00:50:17 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.01%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+         while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+         }
+         return -1;
+    }
+}
