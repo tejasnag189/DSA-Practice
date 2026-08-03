b6bfb6c2a8f27867d34bbf6e5ded9c8824f8eabe
tejasnag189 <tejasnag189@gmail.com>
2026-08-03 15:10:36 +0530

    Time: 3 ms (66.9%), Space: 47.7 MB (49.77%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+	     int i=0;
+	     while(i<nums.length) {
+	    	 int correct=nums[i]-1;
+	    	 if(nums[i]!=nums[correct]) {
+	    		 swap(nums,i,correct);
+	    	 }else {
+	    		 i++;
+	    	 }
+	     }
+	     for(int index=0;index<nums.length;index++) {
+	    	 if(nums[index]!=index+1) {
+	    		 return new int[]{nums[index],index+1};
+	    	 }
+	     }
+	     return new int[] {-1,-1};
+	    }
+	 static void swap(int[]arr,int first,int last) {
+			int temp=arr[first];
+			arr[first]=arr[last];
+			arr[last]=temp;
+	 }
+}
